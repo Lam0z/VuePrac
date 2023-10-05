e56ff3a9f22378d34fe52af46ad03c110d5b5d4a
Nick Efimov <lamoz760@gmail.com>
2023-10-05 17:32:22 +0300

    Small changes

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="nav justify-content-center">
+        <a class="nav-link active" href="#" aria-current="page">Active link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link disabled" href="#">Disabled link</a>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
