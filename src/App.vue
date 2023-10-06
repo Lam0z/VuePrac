4c5a222c5e4908117ec6dd0020e23ddf4adc6d38
Nick Efimov <lamoz760@gmail.com>
2023-10-06 17:34:24 +0300

    Add tabs b-5

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-    <app-header />
+  <app-tabs />
 </template>
 
 <script>
-import appHeader from "./components/AppHeader.vue";
+import AppTabs from "./components/AppTabs.vue";
 
 export default {
-    name: "App",
-    components: { appHeader },
+  name: "App",
+  components: { AppTabs },
 };
 </script>
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/AppTabs.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div class="tabs d-flex justify-content-start text-start">
+      <div class="tabs-item" v-for="(item, idx) in tabsContent" :key="idx">
+        <a
+          @click="choose(idx)"
+          class="tabs-link text-decoration-none d-flex p-3"
+          :class="{ active: item.active }"
+          href="#"
+          >{{ item.title }}</a
+        >
+      </div>
+    </div>
+    <div class="tabs-content-item p-3 border-top border-primary text-start">
+      {{ tabsContent[curIndex].descr }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      curIndex: 0,
+      tabsContent: [
+        {
+          title: "Active",
+          descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          href: "",
+          active: true,
+        },
+        {
+          title: "Link",
+          descr:
+            "Quaerat at eius asperiores sed minus dignissimos est iusto nemo.",
+          href: "",
+          active: false,
+        },
+        {
+          title: "Link",
+          descr: "Vero veritatis veniam laborum excepturi reprehenderit?",
+          href: "",
+          active: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    choose(i) {
+      this.curIndex = i;
+      this.tabsContent.forEach((i) => {
+        i.active = false;
+      });
+      this.tabsContent[i].active = true;
+    },
+  },
+};
+</script>
+
+<style>
+.tabs-link {
+  transition: 0.2s all ease-in;
+}
+.tabs-link.active {
+  background: #d4d4d4;
+  color: white;
+}
+.tabs-link:hover {
+  background: #d4d4d4;
+}
+</style>
